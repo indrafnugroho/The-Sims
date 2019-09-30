6d4f0bb7066c44b91118487bb466cb63bc743030
Indra F Nugroho <indrafbrngrh@gmail.com>
2019-09-30 22:12:11 +0700

    revisi main.c

--- main.c
+++ main.c
@@ -9,62 +9,63 @@ int main () {
     while (!IsFinal(s)) {
         ShowStats(s); printf("\n");
         printf("What will you do? (type answer in Bahasa)\n");
-        scanf("%s", inwords);
-        switch (inwords) {
-            case "Tidur Siang" :
-                Nap(&s);
-                break;
-            case "Tidur Malam" :
-                Sleep(&s);
-                break;
-            case "Makan Hamburger" :
-                EatHamburger(&s);
-                break;
-            case "Makan Pizza" :
-                EatPizza(&s);
-                break;
-            case "Makan Steak and Beans" :
-                EatSteaknBeans(&s);
-                break;
-            case "Minum Air" :
-                DrinkWater(&s);
-                break;
-            case "Minum Kopi" :
-                DrinkCoffee(&s);
-                break;
-            case "Minum Jus" :
-                DrinkJuice(&s);
-                break;
-            case "Buang Air Kecil" :
-                PeePee(&s);
-                break;
-            case "Buang Air Besar" :
-                PooPoo(&s);
-                break;
-            case "Bersosialisasi ke Kafe" :
-                GoToCafe(&s);
-                break;
-            case "Bermain Media Sosial" :
-                SocMed(&s);
-                break;
-            case "Bermain Komputer" :
-                PlayComputer(&s);
-                break;
-            case "Mandi" :
-                TakeAShower(&s);
-                break;
-            case "Cuci Tangan" :
-                WashHands(&s);
-                break;
-            case "Mendengarkan Musik di Radio" :
-                ListenToRadio(&s);
-                break;
-            case "Membaca Koran" :
-                ReadNewspaper(&s);
-                break;
-            case "Membaca Novel" :
-                ReadNovel(&s);
-                break;
+        scanf("%s", &inwords);
+        if (inwords == "Tidur Siang") {
+            Nap(&s);
+        }
+        else if (inwords == "Tidur Malam") {
+            Sleep(&s);
+        }
+        else if (inwords == "Makan Hamburger") {
+            EatHamburger(&s);
+        }
+        else if (inwords == "Makan Pizza") {
+            EatPizza(&s);
+        }
+        else if (inwords == "Makan Steak and Beans") {
+            EatSteaknBeans(&s);
+        }
+        else if (inwords == "Minum Air") {
+            DrinkWater(&s);
+        }
+        else if (inwords == "Minum Kopi") {
+            DrinkCoffee(&s);
+        }
+        else if (inwords == "Minum Jus") {
+            DrinkJuice(&s);
+        }
+        else if (inwords == "Buang Air Kecil") {
+            PeePee(&s);
+        }
+        else if (inwords == "Buang Air Besar") {
+            PooPoo(&s);
+        }
+        else if (inwords == "Bersosialisasi ke Kafe") {
+            GoToCafe(&s);
+        }
+        else if (inwords == "Bermain Media Sosial") {
+            SocMed(&s);
+        }
+        else if (inwords == "Bermain Komputer") {
+            PlayComputer(&s);
+        }
+        else if (inwords == "Mandi") {
+            TakeAShower(&s);
+        }
+        else if (inwords == "Cuci Tangan") {
+            WashHands(&s);
+        }
+        else if (inwords == "Mendengarkan Musik di Radio") {
+            ListenToRadio(&s);
+        }
+        else if (inwords == "Membaca Koran") {
+            ReadNewspaper(&s);
+        }
+        else if (inwords == "Membaca Novel") {
+            ReadNovel(&s);
+        }
+        else {
+            printf("You put the wrong input. Try again\n");
         }
     }
 
